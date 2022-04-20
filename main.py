cc6f675f548c55f172b135eeca9da159fc113f5c
Mirko Matošin <llcoolmirko@hotmail.com>
2022-04-20 15:21:16 +0200

    Paar Sachen mit der fft ausprobiert und geplottet. Noch nicht kapiert. Wir machen erstmal den Schnitt anhand der Infos im Zeitbereich: Energie des Signals und dessen Ableitungen
    Signed-off-by: Mirko Matošin <llcoolmirko@hotmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # @Author Mirko Matosin
 # @Date 14.02.2022
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import scipy.io
-import math
-from scipy import stats
-import matplotlib.pyplot as plt
+
 plt.rcParams['figure.dpi'] = 150
-#plt.rcParams['figure.figsize'] = (12, 7)
+# plt.rcParams['figure.figsize'] = (12, 7)
 from scipy.io import wavfile
-from os.path import dirname, join as pjoin
+from scipy import signal
+
+from os.path import join as pjoin
 from os import listdir
 
 '''
@@ -19,9 +19,6 @@ https://www.programcreek.com/python/example/93227/scipy.io.wavfile.read
 https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
 https://klyshko.github.io/teaching/2019-02-22-teaching
 
-'''
-
-'''
 Digital sound
 
 When you hear a sound your ear’s membrane oscillates because the density and pressure of the air in close proximity 
@@ -31,9 +28,17 @@ is encoded as numerical samples in continuous sequence. For example, in CD (or W
 per second each with 16 bit sample depth, i.e. there are 2^16 = 65536 possible values of the signal: from -32768 to 32767. 
 For the example below, a sound wave, in red, represented digitally, in blue (after sampling and 4-bit quantization).
 
+
+
+2f/2s Signal    ?????
+
+To Read:
+https://dewesoft.com/daq/guide-to-fft-analysis
+
 '''
 
-def plotSounds(data, filename, samplerate = 48000, xlab = 'Time [s]', ylab='Amplitude'):
+
+def plotSounds(data, filename, samplerate=48000, xlab='Time [s]', ylab='Amplitude'):
     """
     Plottet eine Spalte oder ein Vektor in Timedomain
 
@@ -50,8 +55,8 @@ def plotSounds(data, filename, samplerate = 48000, xlab = 'Time [s]', ylab='Ampl
     plt.legend(loc='upper right')
     plt.grid(True)
     plt.xscale('linear')
-    plt.yscale('linear')
-    plt.xlim(xmax=2.01, xmin = -0.01)
+    plt.yscale('log')
+    # plt.xlim(xmax=1.31, xmin=1)
     plt.xlabel(xlab)
     plt.ylabel(ylab)
     plt.show()
@@ -81,11 +86,75 @@ if __name__ == "__main__":
     #
     print('hello World')
 
-    audio_dir = (r'rohdaten/') # r steht für roh/raw.. Damit lassen sich windows pfade verarbeiten
-    files, df = getWavFromFolder(wavdir=audio_dir) # ich mag explizite Programmierung. Also wavdir=...,
+    audio_dir = (r'rohdaten/')  # r steht für roh/raw.. Damit lassen sich windows pfade verarbeiten
+    files, df = getWavFromFolder(wavdir=audio_dir)  # ich mag explizite Programmierung. Also wavdir=...,
     # damit sehen wir sofort welche variable wie verarbeitet wird. Erleichtert die Lesbarkeit.
-
+    from scipy.fft import fft, fftfreq
 
     # ergibt ein Tupel aus Spaltenname und Serie für jede Spalte im Datenrahmen:
     for (columnName, columnData) in df.iteritems():
-        plotSounds(data=columnData, filename=columnName)
+        if not '26' in columnName:
+            continue
+        else:
+            plotSounds(data=columnData, filename=columnName)
+            # yf = fft.fft(columnData)
+            # xf = fft.fftfreq(yf)
+            # Number of sample points
+
+            N = 44000
+            # sample spacing
+            T = 1.0 / N / 10
+            print(T)
+            x = np.linspace(start=0.0, stop=20e3, num=N, endpoint=False)
+            y = columnData.to_numpy()
+            yf = fft(y)
+            print(yf)
+            print(np.abs(yf))
+            xf = fftfreq(N, T)[:N // 2]
+
+            plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
+            plt.xlim(xmax=4e3, xmin=0)
+            # plt.legend(loc='upper right')
+            plt.xlabel(columnName)
+            plt.grid()
+
+            plt.show()
+
+            # Autokorrelation: Verstärkung des Signals?
+            autocorr = signal.convolve(y,y)
+            plt.plot(autocorr)
+            plt.show()
+            break
+
+'''
+FFT ist nun also die Summe aller sinus Funktionen normalisiert auf die Menge aller Datenpunkte N. 
+
+$A(f_k) = \frac{1}{N} \sum_{n=0}^{N-1} a(t_n) e^{-i \frac{2 \pi kn}{N}}$
+
+'''
+
+'''
+Das Ziel ist nun die wesentlichen Signalanteile in der Wav zu finden. Dies erfordert das Nutzen der FFT. Die FFT 
+wird das Signal in seine spektralen Bestandteile zerlegen. 
+
+Nullhypothese:
+Hypothese ist das ein lautes Grundrauschen bis ca. 800 Hz enthalten ist. Das Ticken nach unten (0-30s) selbst sollte im 
+Frequenzbereich um 1k Hz liegen. Dies können wir nutzen um das Ticken zu isolieren und zu verstärken
+
+Alternativhypothese: 
+Es gibt keinen Unterschied von hoch/runter.. Die Eigenschwingung \omega_E,R ist stets konstant.
+
+Vorgehen:
+Der erste Schritt besteht darin, die Eingangszeitdaten in FFT-Zeitblöcke zu zerlegen. Die Eingangszeitdaten können rohe 
+Sensorsignale oder vorverarbeitete (z. B. gefilterte) Signale sein. Jeder Zeitblock hat eine Zeitdauer T, die sich auf 
+die spektrale Auflösung der erzeugten Spektren bezieht. Die Zeitblöcke können so konfiguriert werden, dass eine 
+Fensterfunktion angewandt wird und ein Überlappungssatz entsteht. 
+Anschließend werden die FFT-Zeitblöcke mit Hilfe des FFT-Algorithmus vom Zeitbereich in den Frequenzbereich transformiert. 
+Jeder Zeitblock ergibt ein momentanes komplexes FFT-Spektrum. 
+Die momentanen komplexen FFT-Spektren werden zur Berechnung der momentanen Leistungsspektren verwendet. Die Leistungsspektren 
+werden über eine bestimmte Anzahl von Spektren oder eine bestimmte Zeitdauer gemittelt. Leistungsspektren haben reelle 
+Werte und beziehen sich auf ein Eingangssignal. Kreuzleistungsspektren haben komplexe Werte und beziehen sich auf zwei 
+Eingangssignale.
+
+
+'''
