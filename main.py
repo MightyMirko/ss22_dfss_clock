e644e676600bc40cb95f19259b43c3dc966649a3
Mirko Matošin <llcoolmirko@hotmail.com>
2022-05-18 15:59:41 +0200

    Testmode und Kohärenz eingebaut - kohärenz mag noch nicht : https://de.wikipedia.org/wiki/Koh%C3%A4renz_(Signalanalyse)
    Signed-off-by: Mirko Matošin <llcoolmirko@hotmail.com>

--- main.py
+++ main.py
@@ -16,27 +16,63 @@ from scipy.signal import butter
 from scipy.signal import sosfilt
 
 
-def fft_test(ax2d, data, sample_rate=48000, fourier_abtastzeit=0.1, do_plot=False, filename='test'):
+def kohaerenz(axs):
+    '''
+    Testphase... Weiß nicht ob notwendig.. Mir fehlt da noch die Theorie
+    https://de.wikipedia.org/wiki/Koh%C3%A4renz_(Signalanalyse)
+    https://matplotlib.org/stable/gallery/lines_bars_and_markers/cohere.html#sphx-glr-gallery-lines-bars-and-markers-cohere-py
+    :param axs:
+    :return:
+    '''
+
+    s1 = filtered
+    s2 = moved_average_data
+    dt = 1 / 96000
+    print(type(s1), '\n', type(s2))
+    print(s1.shape, '\n', s2.shape)
+
+    axs[2].plot(time, s1, time, s1)
+    axs[2].set_xlim(0, 2)
+    axs[2].set_xlabel('time')
+    axs[2].set_ylabel('s1 and s2')
+    axs[2].grid(True)
+
+    cxy, f = axs[3].cohere(s1, s1, 256, 1. / dt)
+    # axs[3].set_ylabel('coherence')
+    out = axs
+
+
+def fft_test(ax2d, data, sample_rate=48000, do_plot=False, filename='test'):
+    '''
+
+    :param ax2d: Matplotlib artist als mehrdimensionale Variable
+    :param data: Die Daten für die FFT
+    :param sample_rate: Standard ist 48kHz - kann geändert werden (ungetestet)
+    :param do_plot: Wenn die Funktion gleich auch plotten soll? Achtung: Rechner stürzt bei vielen Figures ab
+    :param filename: Aktueller Dateiname
+    :return: Fourierdaten und das Matplotlibhandle zur weiteren Kontrolle der Plots.
+    '''
     signal_length = 2
     # sample spacing
     x = np.linspace(start=0.0, stop=2, num=signal_length * sample_rate, endpoint=False)
     y = data.to_numpy()
 
     yf = fft(y)
-    fourier_abtastzeit  = 1 / 10 / 48000
+    fourier_abtastzeit = 1 / 10 / 48000
     xf = fftfreq(sample_rate, fourier_abtastzeit)[:sample_rate // 2]
     print(xf)
 
     out = ''
     if do_plot:
-        ax  = ax2d[0]
+        ax = ax2d[0]
         ax1 = ax2d[1]
         apfel = np.abs(yf[0:sample_rate // 2])
         # Normalisierung?
         normale = (2.0 / sample_rate * apfel)
         out = ax.plot(xf, normale)
-        # plt.legend(loc='upper right')
-        ax.set_xlabel('Dateiname:\n' + filename)
+        ax.set_title('Fourier Transformation in linear und log bis 10k')
+        ax.set_xlabel('Frequency')
+        ax.set_ylabel('Amplitude linear')
         ax.set_xlim(left=0, right=1e4)
         ax.grid()
 
@@ -45,6 +81,8 @@ def fft_test(ax2d, data, sample_rate=48000, fourier_abtastzeit=0.1, do_plot=Fals
         ax1.set_xlabel('Dateiname:\n' + filename)
         ax1.set_xlim(left=0, right=1e4)
         ax1.set_yscale('log')
+        ax1.set_ylabel('Amplitude in dB')
+
         ax1.grid()
 
     fourier = [yf, xf]
@@ -52,9 +90,13 @@ def fft_test(ax2d, data, sample_rate=48000, fourier_abtastzeit=0.1, do_plot=Fals
 
 
 def mov_avg(data_filtered, timevec, do_plot=False):
-    ''' Moving Average-Filter
-        Achtung!!! Anzahl an Sampels wird um k-1 verringert '''
-
+    '''
+    Moving Average-Filter.. Achtung!!! Anzahl an Sampels wird um k-1 verringert
+    :param data_filtered:
+    :param timevec:
+    :param do_plot:
+    :return:
+    '''
     t = timevec
 
     # Fenstergröße k festlegen (nur ungerade k erlaubt!!!)
@@ -70,7 +112,6 @@ def mov_avg(data_filtered, timevec, do_plot=False):
 
     if do_plot:
         # plot gefiltertes Signal gesamt
-        plt.figure()
         plt.plot(t[(window_size // 2):-(window_size // 2)], moving_averages)
         plt.grid(True)
         plt.xlabel('Zeit in s')
@@ -94,9 +135,19 @@ def mov_avg(data_filtered, timevec, do_plot=False):
 
 
 def buttern(data, timevec, filename='N/A', doplot=False, samplerate=48000, omega=500):
-    ''' Butterworth-Hochpassfilter
+    '''
+       Butterworth-Hochpassfilter
         wn = 150 rad/s bestes Ergebnis für Signal
-        wn = 500 rad/s bestes Ergebnis für Rauschen '''
+        wn = 500 rad/s bestes Ergebnis für Rauschen
+    :param data: Data zum buttern als pandas.series/numpy.array
+    :param timevec: x-achse
+    :param filename: Dateiname
+    :param do_plot: Wenn die Funktion gleich auch plotten soll? Achtung: Rechner stürzt bei vielen Figures ab
+    :param samplerate:
+    :param omega: Wie soll gebuttert werden?
+    :return:
+    '''
+
 
     wn = omega
     # warum braucht es eine so große Ordnung?
@@ -166,15 +217,18 @@ def getWavFromFolder(wavdir):
 if __name__ == "__main__":
     #
     print('hello World')
+    do_plot = False
+    do_test_mode = True
 
     audio_dir = (r'rohdaten/')  # r steht für roh/raw.. Damit lassen sich windows pfade verarbeiten
     files, df = getWavFromFolder(wavdir=audio_dir)  # ich mag explizite Programmierung. Also wavdir=...,
     # damit sehen wir sofort welche variable wie verarbeitet wird. Erleichtert die Lesbarkeit.
     samplerate = 48000
     plt.clf()
+
     # ergibt ein Tupel aus Spaltenname und Serie für jede Spalte im Datenrahmen:
     for (columnName, columnData) in df.iteritems():
-        if not '26' in columnName:
+        if do_test_mode and not '26' in columnName:
             continue
         else:
             # plotSounds(data=columnData, filename=columnName)
@@ -193,11 +247,12 @@ if __name__ == "__main__":
 
             # Logaritmische Darstellung
             plotSounds(ax1, data=columnData, filename=columnName, abszisse=time,
-                       log=True, title='Original', ylab='Amplitude in dB')
+                       log=True, title='Original-Signal', ylab='Amplitude in dB')
 
             plotSounds(ax2, data=columnData, filename=columnName, abszisse=time,
                        log=False, title='')
-            fig2, axs = plt.subplots(2, 1, figsize=(12, 8))
+
+            fig2, axs = plt.subplots(4, 1, figsize=(12, 8))
 
             plotSounds(axs[0], data=filtered, filename=columnName, abszisse=time,
                        title='Gebuttert mit $\omega$= %i' % omega_butter, log=False)
@@ -209,8 +264,8 @@ if __name__ == "__main__":
                        title='Moving AVG', log=False)
 
             # Buttern, dann filtern
-            fig1.show()
-            fig2.show()
+            fig1.tight_layout()
+            fig2.tight_layout()
 
             fig3, axs = plt.subplots(2, 1, figsize=(12, 8))
 
@@ -224,17 +279,25 @@ if __name__ == "__main__":
             plotSounds(axs[1], data=moved_average_data, filename=columnName, abszisse=time[0:95996],
                        title='Moving AVG', log=False)
 
-            fig3.show()
-            plt.close(fig3)
-            plt.close(fig2)
-            plt.close(fig1)
+            fig3.tight_layout()
+            # fig3.show()
 
             fig4, axs = plt.subplots(2, 1, figsize=(12, 8))
-            fft = fft_test( ax2d=axs, data=columnData, do_plot=True)
+            fft = fft_test(ax2d=axs, data=columnData, do_plot=True)
             print(fft[0])
 
-            fig4.show()
-            plt.close(fig4)
+            if do_plot:
+                fig4.tight_layout()
+                fig3.show()
+                fig4.show()
+                fig1.show()
+                fig2.show()
+                plt.close(fig4)
+                plt.close(fig3)
+                plt.close(fig2)
+                plt.close(fig1)
+            else:
+                print('Keine Plots erwünscht')
             break
 
     print('bye world')
