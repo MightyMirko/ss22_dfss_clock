fd1eb2b98dd9ef1c83753b774cbdae8a406c0c32
Alexander <wual1020@h-ka.de>
2022-06-07 10:52:19 +0200

    Energie-Berechnung absolut Teil 2

--- Energie.py
+++ Energie.py
@@ -19,7 +19,7 @@ from scipy.signal import hilbert
 from scipy.fft import fft, fftfreq
 from scipy.signal import windows
 from scipy.signal import medfilt
-
+from scipy.stats import norm, t, chi2, f
 
 #%%
 
@@ -37,7 +37,6 @@ for idx in range(len(file_list)):
     signal_2 = data**2
     energie[idx] = signal_2.sum()
 
-
 # plot Energie der Signale
 plt.figure()
 plt.plot(energie)
@@ -49,4 +48,40 @@ plt.show()
 
 #%%
 
-''' weitere Analysen der Ausreiser '''
+''' weitere Analysen '''
+
+data = energie
+N = np.size(data)
+data_mean = np.mean(data)
+data_var = np.var(data, ddof = 1)
+data_std = np.std(data, ddof = 1)
+Einheit_unit = ''
+
+# Unbekannter Mittelwert, Unbekannte Varianz - t-Verteilung mit N - 1 FG
+gamma = 0.9973
+c1 = t.ppf((1 - gamma) / 2, N - 1)
+c2 = t.ppf((1 + gamma) / 2, N - 1)
+x_prog_min = data_mean + c1 * data_std * np.sqrt(1 + 1 / N)
+x_prog_max = data_mean + c2 * data_std * np.sqrt(1 + 1 / N)
+print('Prognosewert:', round(x_prog_min, 4), Einheit_unit, '< x <=', round(x_prog_max, 4), Einheit_unit)
+
+#%%
+
+N = len(energie)
+x = np.arange(N)
+y = np.ones(N)
+# plot Energie der Signale
+plt.figure()
+plt.plot(x, energie)
+plt.grid(True)
+plt.xlabel('Anzahl Aufnahmen')
+plt.ylabel('Energie')
+plt.title('absolute Energie der Aufnahmen')
+
+# plot Prognosebereich
+plt.plot(x, y*x_prog_min)
+plt.plot(x, y*x_prog_max)
+plt.plot(x, y*np.median(energie))
+plt.plot(x, y*np.mean(energie))
+plt.grid(True)
+plt.show()
