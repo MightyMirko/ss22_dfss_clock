e3883d0ba4997e97f43fa0e4614f07526ba5568b
Alexander <wual1020@h-ka.de>
2022-06-11 14:04:54 +0200

    Prognoseintervall bestimmt

--- Energie.py
+++ Energie.py
@@ -3,103 +3,122 @@
 Created on Sun Jun  5 16:42:31 2022
 
 @author: Alexander
+
+Energie-Berechnung und
+Schwellwert-Abschätzung für Energie-Filter
 """
 
-# Bibliotheken
-import numpy as np
-import pandas as pd
+#Bibliotheken
+import numpy as np 
+import pandas as pd 
 import matplotlib.pyplot as plt
 import os
 
 from scipy.io import wavfile
-# from scipy import signal as sig
+#from scipy import signal as sig
 from scipy.signal import butter
 from scipy.signal import sosfilt
 from scipy.signal import hilbert
 from scipy.fft import fft, fftfreq
 from scipy.signal import windows
 from scipy.signal import medfilt
-from scipy.stats import norm, t, chi2, f
-from tqdm.notebook import tqdm
+from scipy.stats import norm, t, chi2, f, weibull_min, gamma
 
-# %%
-if os.path.exists('H:\Messung_BluetoothMikro\Messung 3\Audios'):
-    path_audio = r'H:\Messung_BluetoothMikro\Messung 3\Audios'
+#%%
+
+# Pfad auf das Verzeichniss
+path_audio = r'data/Neuer Ordner'
+# Liste der Dateien erstellen
 file_list = os.listdir(path_audio)
-file_list = file_list[100:9000]
-number_of_files = len(file_list)
-#p = tqdm(total=number_of_files, disable=False)
 
-print(number_of_files)
+# Manuelle Filterung der Daten
+# Schlechte Dateien aussortieren
+file_list.remove('20220510_02_27_09.wav')
+file_list.remove('20220510_05_30_45.wav')
+file_list.remove('20220510_05_48_54.wav')
+file_list.remove('20220510_09_28_01.wav')
+file_list.remove('20220510_09_28_07.wav')
+file_list.remove('audio_for_test.wav')
+file_list.remove('ausreißer_mit_tick.wav')
+
+# Anzahl der Dateien ermitteln
+number_of_files = len(file_list)
+# Ergebnisspeicher allokieren
 energie = np.zeros(number_of_files)
 
-# Energie bestimmen absolut
+# absolute Energie bestimmen
 for idx in range(len(file_list)):
-    # einlesen
+    #einlesen
     samplerate, data = wavfile.read(path_audio + '/' + file_list[idx])
+    
+    # plot
+    plt.figure()
+    plt.plot(data)
+    plt.grid(True)
+    plt.title(file_list[idx])
+    plt.show()
+    
     # Berechnung
-    signal_2 = data ** 2
+    signal_2 = data**2
     energie[idx] = signal_2.sum()
-    if idx % 100 == 0:
-        print(idx)
 
-print('Fertig:')
-
-# %%
 # plot Energie der Signale
-plt.rcParams['figure.dpi'] = 300
-# plt.rcParams['interactive'] = True
-plt.rcParams['figure.figsize'] = (12, 9)
-
 plt.figure()
-# plt.scatter(x=,y=energie)
-# plt.xlim(xmin=20, xmax=30)
 plt.plot(energie)
-
 plt.grid(True)
 plt.xlabel('Anzahl Aufnahmen')
 plt.ylabel('Energie')
 plt.title('absolute Energie der Aufnahmen')
 plt.show()
 
-# %%
+#%%
+
+''' Histogramm '''
+
 X = energie
-h_abs, x = np.histogram(X, bins=1000)
+h_abs, x = np.histogram(X, bins=50)
 
 ax = plt.figure(figsize=(6, 4)).subplots(1, 1)
-ax.hist(X, x, histtype='bar', weights=np.ones(X.shape) / X.shape, rwidth=0.9, cumulative=False)
-ax.grid(True, which='both', axis='both', linestyle='--')
+ax.hist(X, x , histtype='bar', weights=np.ones(X.shape)/X.shape, rwidth=0.9, cumulative=False)
+ax.grid(True, which= 'both', axis='both', linestyle='--')
 ax.set_xlabel('Energie')
-ax.set_ylabel('Häufigkeit h(d)')
-# ax.set_ylim(top=0.001)
-# ax.set_xlim(left=10, right=1000)
-
-ax.set_title('Häufigkeitsverteilung')
+ax.set_ylabel('Häufigkeit h(E)')
+#ax.set_ylim(top=0.25)
+#ax.set_title('Kommulative Häufigkeitsverteilung')
 plt.tight_layout()
 
-plt.show()
 
-# %%
 
-''' weitere Analysen '''
+#%%
+
+''' Prognosebereich bestimmen '''
 
 # Berechnung des Prognosebereichs
 data = energie
 N = np.size(data)
 data_mean = np.mean(data)
-data_var = np.var(data, ddof=1)
-data_std = np.std(data, ddof=1)
+data_var = np.var(data, ddof = 1)
+data_std = np.std(data, ddof = 1)
 Einheit_unit = ''
 
+'''
 # Unbekannter Mittelwert, Unbekannte Varianz - t-Verteilung mit N - 1 FG
-gamma = 0.95
+gamma = 0.95# 0.9973
 c1 = t.ppf((1 - gamma) / 2, N - 1)
 c2 = t.ppf((1 + gamma) / 2, N - 1)
 x_prog_min = data_mean + c1 * data_std * np.sqrt(1 + 1 / N)
 x_prog_max = data_mean + c2 * data_std * np.sqrt(1 + 1 / N)
 print('Prognosewert:', round(x_prog_min, 4), Einheit_unit, '< x <=', round(x_prog_max, 4), Einheit_unit)
+'''
 
-# %%
+# Unbekannter Mittelwert, Unbekannte Varianz - t-Verteilung mit N - 1 FG
+gamma = 0.95# 0.9973
+c = t.ppf((gamma), N - 1)
+x_prog_max = data_mean + c * data_std * np.sqrt(1 + 1 / N)
+print('Prognosewert:', x_prog_max)
+
+
+#%%
 
 ''' Plots '''
 N = len(energie)
@@ -114,35 +133,45 @@ plt.ylabel('Energie')
 plt.title('absolute Energie der Aufnahmen')
 
 # plot Prognosebereich
-# plt.plot(x, y*x_prog_min)
-plt.plot(x, y * x_prog_max)
-plt.plot(x, y * np.median(energie))
-plt.plot(x, y * np.mean(energie))
+#plt.plot(x, y*x_prog_min)
+plt.plot(x, y*x_prog_max)
+plt.plot(x, y*np.median(energie))
+plt.plot(x, y*np.mean(energie))
 plt.grid(True)
 plt.show()
 
-# %%
-plt.figure()
-plt.scatter(x, energie,color='b')
-plt.grid(True)
-plt.xlabel('Anzahl Aufnahmen')
-plt.ylabel('Energie')
-plt.title('absolute Energie der Aufnahmen')
-# plt.show()
-# %%
-birne = np.max(energie)
-apfel = np.argmax(energie)
-faulerapfel = file_list[int(apfel)]
+#%%
 
-# %%
-#
-energie[int(apfel)] = 0
-# %%
-# plot Prognosebereich
-# plt.plot(x, y*x_prog_min)
-plt.plot(x, y * x_prog_max, color='green', linewidth=5, label='Max Prognose')
-plt.plot(x, y * np.median(energie),color='k',label='Median')
-plt.plot(x, y * np.mean(energie), color='r', label='ar.Mittelwert')
-plt.grid(True)
-plt.legend()
-plt.show()
+''' Approximation als Weibull-Verteilung '''
+
+x = np.arange(0,8,0.1)     # x manuell festlegen
+eta = 1.1
+beta = 1.1#np.array([0, 0.5, 1, 1.5, 2, 2.5, 3])
+WB = weibull_min.pdf(x, c=beta, loc=0.25, scale=eta)
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, WB)
+ax.grid(True)
+ax.set_xlabel("Energie")
+ax.set_ylabel("Häufigkeitsverteilung")
+
+#%%
+
+''' Approximation als Gamma-Verteilung '''
+
+x = np.arange(0,8,0.1)     # x manuell festlegen
+b = 1.1
+p = 1.1#np.array([0, 0.5, 1, 1.5, 2, 2.5, 3])
+gam = gamma.pdf(x, a=p, loc=0.25, scale=b)
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, gam)
+ax.grid(True)
+ax.set_xlabel("Energie")
+ax.set_ylabel("Häufigkeitsverteilung")
+
+
+#%%
+
+''' Approximation als Chi2-Verteilung '''
+
